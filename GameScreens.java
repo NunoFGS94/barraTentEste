02de96e7aa61b36962e1f7b06daeb4dabaf19738
NunoFGS94 <47813571+NunoFGS94@users.noreply.github.com>
2019-02-20 19:55:56 +0000

    Add files via upload
    utilities

---
+++ GameScreens.java
@@ -0,0 +1,19 @@
+package org.academiadecodigo.bootcamp;
+
+/**
+ * Created by codecadet on 20/02/2019.
+ */
+public abstract class GameScreens {
+
+    public abstract boolean action();
+
+    public abstract boolean isUnlocked();
+
+    public abstract void setUnlocked();
+
+    public abstract void drawStage();
+
+    public abstract void hideStage();
+
+
+}
